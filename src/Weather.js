247b03223d2a5c63bc732b31daf78c114d6fd029
Mark McNally <mark@macanalytics.ie>
2022-10-05 09:58:31 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 
 
+
 export default function Weather(props) {
     function handleResponse(response) {
         alert (`the weather in ${response.data.name} is ${response.data.main.temp}°C`)
@@ -10,5 +11,5 @@ export default function Weather(props) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(handleResponse);
-return <h2> Hello from Weather</h2>;
+return <h2> Hello from React</h2>;
 }
